e0ca6b30b9cb86a148afbcd2af853c2894af3088
Caiya Varden <caiya.varden@ellefsontech.com>
2025-07-11 21:41:59 -0300

    remove vite.svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { Suspense, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
